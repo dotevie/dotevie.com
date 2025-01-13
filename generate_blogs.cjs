f1b56869220ab21e13b68826306cdbde46847c1f
sayofthelor <justsailor06@gmail.com>
2025-01-13 16:53:39 -0500

    automatic article sort by date
    this changes absolutely nothing on the client side but it'll make it a lot easier once there are more articles on this page

--- generate_blogs.cjs
+++ generate_blogs.cjs
@@ -6,6 +6,27 @@ const path = require('path');
 // Bad JavaScript Practices: The File
 // you can take this if you want but idk how useful it's gonna be lol
 
+function manualDateParse(date) {
+    // i know this is stupid
+    // but Date.toLocaleDateString wouldn't spit out the format i wanted
+    const list = date.split("-");
+    const year = parseInt(list[0]);
+    const month = parseInt(list[1]);
+    const day = parseInt(list[2]);
+    const realMonth = ['', 
+        'january', 'february', 'march',
+        'april', 'may', 'june',
+        'july', 'august', 'september',
+        'october', 'november', 'december'
+    ][month];
+    const dayEnd =
+        day == 1 ? "st" :
+        day == 2 ? "nd" :
+        day == 3 ? "rd" :
+        "th";
+    return realMonth + " " + day + dayEnd + ", " + year;
+}
+
 (function() {
     const htmlTemplate = `<!DOCTYPE html>
 <html>
@@ -49,42 +70,53 @@ const path = require('path');
     </body>
 </html>`;
     let add = "";
-    const files = fs.readFileSync("blogs/source/_order.meta", 'utf-8').split("\n");
-    for (let i = 0; i < files.length; i++) {
-        files[i] = files[i].split("\r")[0]; // fix for windows
+    const files = fs.readdirSync("blogs/source/");
+    for (let i = files.length - 1; i >= 0; i--) {
+        if (files[i].endsWith("meta")) continue;
+        else files.splice(i, 1);
     }
+    files.sort((a,b) => {
+        const reala = fs.readFileSync("blogs/source/"+a, 'utf-8');
+        const realb = fs.readFileSync("blogs/source/"+b, 'utf-8');
+        const contents = [reala.split("\n"), realb.split("\n")];
+        return new Date(contents[1][1].split("\r")[0]) - new Date(contents[0][1].split("\r")[0]);
+    });
+    for (let i = 0; i < files.length; i++) {
+        let fname = files[i].split(".");
+        fname[fname.length - 1] = "md";
+        files[i] = fname.join(".");
+    }
+    console.log(files);
     for (const file of files) {
-        if (file.endsWith("md")) {
-            console.log(`parsing ${file}...`);
-            const pth = path.parse(file).name;
-            let name = "";
-            let date = null;
-            if (fs.existsSync(`blogs/source/${pth}.meta`)) {
-                const meta = fs.readFileSync(`blogs/source/${pth}.meta`, 'utf-8').split("\n");
-                name = meta[0].split("\r")[0]; // fix for windows
-                date = meta[1];
-                console.log(`meta file found for ${file}, usng name "${name}" and date "${date}"`);
-            } else {
-                name = toTitleCase(pth.replace("-", " "));
-                console.log(`no title file found for ${file}, using auto name "${name}"`);
-            }
-            add += `\n\t\t\t<li><div style="display: flex; justify-content: space-between; padding:0;"><a href="${pth+".html"}">${name}</a>${date ?`<p style="padding-right: 40px; font-weight: normal; align-self: right; margin: 0; font-size: medium; text-align: right;">${date}</p>` : ""}</div></li>`;
-            const md = fs.readFileSync(`blogs/source/${file}`, 'utf-8');
-            const html = converter.makeHtml(md);
-            fs.writeFileSync(`blogs/${pth}.html`, htmlTemplate.replace("{{TITLE}}", name)
-            .replace("{{HEADER}}",`<div style="display: flex; justify-content: space-between; padding:0;">
-            <h1>${name}</h1>`
-            + (date ? `    <p class="date">${date}</p>` : "")
-            + "\t\t</div><hr />")
-            .replace("{{SLOT}}", 
-                html.replace("<wobble>", "<div id=\"wobble\">")
-                .replace("</wobble>", "</div>")
-                .replace("<center>", "<div align=\"center\">") // idk if this is the best way to center a div but it's ok for what i'm doing
-                .replace("</center>", "</div>")
-            .split("\n")
-            .join("\n\t\t")
-            ));
+        console.log(`parsing ${file}...`);
+        const pth = path.parse(file).name;
+        let name = "";
+        let date = null;
+        if (fs.existsSync(`blogs/source/${pth}.meta`)) {
+            const meta = fs.readFileSync(`blogs/source/${pth}.meta`, 'utf-8').split("\n");
+            name = meta[0].split("\r")[0]; // fix for windows
+            date = manualDateParse(meta[1]);
+            console.log(`meta file found for ${file}, usng name "${name}" and date "${date}"`);
+        } else {
+            name = toTitleCase(pth.replace("-", " "));
+            console.log(`no title file found for ${file}, using auto name "${name}"`);
         }
+        add += `\n\t\t\t<li><div style="display: flex; justify-content: space-between; padding:0;"><a href="${pth+".html"}">${name}</a>${date ?`<p style="padding-right: 40px; font-weight: normal; align-self: right; margin: 0; font-size: medium; text-align: right;">${date}</p>` : ""}</div></li>`;
+        const md = fs.readFileSync(`blogs/source/${file}`, 'utf-8');
+        const html = converter.makeHtml(md);
+        fs.writeFileSync(`blogs/${pth}.html`, htmlTemplate.replace("{{TITLE}}", name)
+        .replace("{{HEADER}}",`<div style="display: flex; justify-content: space-between; padding:0;">
+        <h1>${name}</h1>`
+        + (date ? `    <p class="date">${date}</p>` : "")
+        + "\t\t</div><hr />")
+        .replace("{{SLOT}}", 
+            html.replace("<wobble>", "<div id=\"wobble\">")
+            .replace("</wobble>", "</div>")
+            .replace("<center>", "<div align=\"center\">") // idk if this is the best way to center a div but it's ok for what i'm doing
+            .replace("</center>", "</div>")
+        .split("\n")
+        .join("\n\t\t")
+        ));
     }
     console.log("writing index.html...");
     fs.writeFileSync("blogs/index.html", htmlTemplate2.replace("{{SLOT}}", add));
--- how_to_use_blogs.md
+++ how_to_use_blogs.md
@@ -3,13 +3,10 @@
 ### 1. write a blog post using markdown syntax into an md file in the `blogs/source` folder
 - special HTML tags \<wobble> and \<center> are allowed to be used here
 ### 2. make a meta file
-- in the same directory, make a file called `(name).meta` that contains the title of the article on one line, and the publish date on another
+- in the same directory, make a file called `(name).meta` that contains the title of the article on one line, and the publish date (yyyy-mm-dd) on another
 - this title will be made into a header on the final article
 - if no title file exists, a title case version of the filename will be used instead and no date will be shown
-- there's no spec you have to follow here for a date but i recommend sticking to something (e.g. `january 1st, 1970`)
-### 3. put your md file into `blogs/source/order.meta`
-- this is to determine the order of the blogs, i would recommend just putting it at the beginning to keep it chromological
-### 4. run the blog generator
+### 44 run the blog generator
 - refer to `README.md` if you forgot how to do this
 
 ### by the end your file structure should look something like this
